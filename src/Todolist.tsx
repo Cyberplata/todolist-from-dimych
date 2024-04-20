4d7b3877b3f654849bb167a0b1e40d1b523240d3
PlatonKynin <kininplaton@gmail.com>
2024-04-20 11:42:24 +0300

    2-In Todolist.tsx add деструктуризацию props и раскидал li с помощью метода map -> i'm

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -13,26 +13,36 @@ type TodolistPropsType = {
 
 export function Todolist(props: TodolistPropsType) {
 
+    const { title, tasks } = props;
+
     return (
         <div>
-            <h3>{props.title}</h3>
+            <h3>{title}</h3>
             <div>
                 <input/>
                 <button>+</button>
             </div>
             <ul>
-                <li>
-                    <input type="checkbox" checked={props.tasks[0].isDone}/>
-                    <span>{props.tasks[0].title}</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={props.tasks[1].isDone}/>
-                    <span>{props.tasks[1].title}</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={props.tasks[2].isDone}/>
-                    <span>{props.tasks[2].title}</span>
-                </li>
+
+                {tasks.map((el, index) => {
+                    return <li key={el.id}>
+                        <input type="checkbox" checked={el.isDone}/>
+                        <span>{el.title}</span>
+                    </li>
+                })}
+
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={tasks[0].isDone}/>*/}
+                {/*    <span>{tasks[0].title}</span>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={tasks[1].isDone}/>*/}
+                {/*    <span>{tasks[1].title}</span>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <input type="checkbox" checked={props.tasks[2].isDone}/>*/}
+                {/*    <span>{props.tasks[2].title}</span>*/}
+                {/*</li>*/}
             </ul>
             <div>
                 <button>All</button>
