b6319501b34f0d3d1fbe5cf769f0569762b882ef
PlatonKynin <kininplaton@gmail.com>
2024-04-20 11:28:47 +0300

    1-in App.tsx add state: tasks1, tasks2, tasks3. In Todolist.tsx add props TasksPropsType

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {TasksPropsType, Todolist} from "./Todolist";
 
 function App() {
 
+    let tasks1: TasksPropsType[] = [
+        { id: 1, title: "CSS&HTML", isDone: true},
+        { id: 2, title: "JS", isDone: true},
+        { id: 3, title: "React", isDone: false}
+    ]
+
+    let tasks2: TasksPropsType[] = [
+        { id: 1, title: "Terminator", isDone: true},
+        { id: 2, title: "XXX", isDone: false},
+        { id: 3, title: "Shutter Island", isDone: true}
+    ]
+
+    let tasks3: TasksPropsType[] = [
+        { id: 1, title: "Terminator", isDone: true},
+        { id: 2, title: "XXX", isDone: false},
+        { id: 3, title: "Shutter Island", isDone: true}
+    ]
+
     return (
         <div className="App">
-            <Todolist title={"What to learn"}/>
-            <Todolist title={"Movies"}/>
-            <Todolist title={"Books"}/>
+            <Todolist title={"What to learn"} tasks={tasks1}/>
+            <Todolist title={"Movies"} tasks={tasks2}/>
+            <Todolist title={"Songs"} tasks={tasks3}/>
         </div>
     );
 }
